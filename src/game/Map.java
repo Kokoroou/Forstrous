f1dc832df5419b0a109226a77905bb25c44467b6
pbching <80338862+pbching@users.noreply.github.com>
2021-06-02 00:05:25 +0700

    Update Map.java

--- src/game/Map.java
+++ src/game/Map.java
@@ -1,5 +1,8 @@
-package game;
+package GameObject;
+
 import java.awt.Color;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
@@ -9,7 +12,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -22,41 +27,195 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Random;
 public class Map extends JLabel {
-	int round = 0;
 	
+	public static final int arr1[][]= {{0,0,0,1,1,1,1,1,1,1,1,1,1,1},//1
+										{0,0,0,1,0,0,0,0,0,0,0,0,1,1},//2
+										{0,0,0,1,0,0,0,0,0,0,0,0,1,1},//3
+										{1,0,0,0,0,0,0,0,0,0,0,0,1,1},//4
+										{1,1,0,0,0,0,0,0,0,0,0,0,1,1},//5
+										{0,0,0,0,0,0,0,0,0,0,0,0,1,1},//6
+										{1,1,1,1,0,0,0,0,0,0,0,0,1,1},//7
+										{1,0,0,0,0,0,0,0,0,0,0,0,0,0},//8
+										{1,0,0,0,0,0,0,1,1,0,0,0,0,0},//9
+										{1,0,0,0,0,0,0,1,1,0,0,0,0,0},//10
+										{1,0,0,0,0,0,0,1,1,0,0,0,0,1},//11
+										{0,0,0,0,0,0,0,0,1,0,0,0,0,1},//12
+										{0,0,0,0,0,0,0,0,0,0,1,1,1,1},//13
+										{0,1,0,0,0,0,0,0,0,0,0,0,1,1},//14
+										{0,1,0,0,0,0,0,0,0,0,0,0,0,1},//15
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},//16
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},//17
+									   	{0,1,1,0,0,0,0,0,0,0,0,0,0,2}};//18
 	
-	public Map() {
-
+	public static final int  arr2[][]= {{0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,1,1,0,0,0,0,0,0,0,1},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+										{0,0,0,1,0,0,0,0,0,0,0,0,1,1},
+										{1,0,0,1,1,1,0,0,0,0,0,0,1,1},
+										{1,0,0,1,1,1,1,0,0,0,0,0,0,1},
+										{1,0,0,1,1,1,1,1,0,0,0,0,0,1},
+										{1,0,0,1,1,1,1,1,0,0,0,0,0,1},
+										{1,0,0,1,1,1,0,0,0,0,0,0,0,1},
+										{1,0,0,1,1,0,0,0,0,0,0,0,0,1},
+										{1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+										{1,1,0,0,0,0,0,0,0,0,0,0,0,1},
+										{1,1,0,0,0,0,0,0,0,0,0,0,1,1},
+									   	{1,1,2,1,1,1,1,1,1,1,1,1,1,1}};
+	
+	public static final int arr3[][]= {{0,0,3,0,0,0,0,0,1,1,1,1,1,1},
+										{0,0,0,0,0,0,1,1,1,1,1,1,1,1},
+										{1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+										{1,1,0,0,0,0,0,0,0,0,0,0,0,1},
+										{1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+										{1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+										{1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+										{1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+										{0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+										{0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+										{0,0,0,0,1,1,0,0,0,0,0,0,0,1},
+									   	{0,0,0,0,0,0,2,0,0,0,0,0,0,0}};
+	
+	public static final int arr4[][]= {{0,0,0,0,0,0,3,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+										{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+									   	{0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+	
+	private int round;
+	
+	private int arr[][];
+	
+	public int[][] getArr() {
+		return this.arr;
 	}
-	public void drawMap(Graphics2D g2d,int n) {
-		Image img;
-		switch (n) {
-		case 0:
-			img= new ImageIcon(getClass().getResource("/Images/map0.png")).getImage();	
-			g2d.drawImage(img, 0, 0, null);
+
+	public void setArr(int round) {
+		switch (this.round) {
+		case 1: 
+			this.arr = arr1;
 			break;
+		case 2:
+			this.arr = arr2;
+			break;
+		case 3:
+			this.arr = arr3;
+			break;
+		case 4:
+			this.arr = arr4;
+			break;
+		default: 
+				break;
+		}
+	}
+
+	public Map() {
+		this.round = 1;
+		setArr(1);
+	}
+	
+	public int getTile(int tileX, int tileY) {
+		return this.arr[tileX][tileY];
+	}
+	
+	public boolean isEndTile(int tileX, int tileY){
+		return (arr[tileX][tileY]==2);
+	}
+	
+	public boolean isBeginTile(int tileX, int tileY){
+		return (arr[tileX][tileY]==3);
+	}
+	
+	public void drawMap(Graphics2D g2d, int round) {
+		BufferedImage img;
+		switch (round) {
 		case 1: {
-			img= new ImageIcon(getClass().getResource("/Images/map1.png")).getImage();	
-			g2d.drawImage(img, 0, 0, null);
+			try {
+				img = ImageIO.read(new File("data/map1.png"));
+				g2d.drawImage(img, 0, 0, null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}	
 			break;
 		}
 		case 2: {
-			img= new ImageIcon(getClass().getResource("/Images/map2.png")).getImage();	
-			g2d.drawImage(img, 0, 0, null);
+			try {
+				img = ImageIO.read(new File("data/map2.png"));
+				g2d.drawImage(img, 0, 0, null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}	
 			break;
 		}
 		case 3: {
-			img= new ImageIcon(getClass().getResource("/Images/map3.png")).getImage();	
-			g2d.drawImage(img, 0, 0, null);
+			try {
+				img = ImageIO.read(new File("data/map3.png"));
+				g2d.drawImage(img, 0, 0, null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}	
 			break;
 		}
 		case 4: {
-			img= new ImageIcon(getClass().getResource("/Images/map4.png")).getImage();	
-			g2d.drawImage(img, 0, 0, null);
+			try {
+				img = ImageIO.read(new File("data/map4.png"));
+				g2d.drawImage(img, 0, 0, null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}	
 			break;
 		}
+		
 		default:
 			break;
+		}
+	}
+	
+	public void update() {
+		
+	}
+	
+	public int getRound() {
+		return round;
+	}
+
+	public void setRound(int round) {
+		this.round = round;
+	}
+
+	public void nextMap() {
+		if(this.getRound() < 4) {
+			this.setRound(getRound()+1);
+			this.setArr(getRound());
+		}
+	}
+	
+	public void backMap() {
+		if(this.getRound() > 1) {
+			this.setRound(getRound()-1);
+			this.setArr(getRound());
 		}
 	}
 }
