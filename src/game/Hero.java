acb9a09f88d45bd99f2537e5f874215cbaeeb1b5
pbching <80338862+pbching@users.noreply.github.com>
2021-06-10 23:39:55 +0700

    Update Hero.java

--- src/game/Hero.java
+++ src/game/Hero.java
@@ -1,70 +1,22 @@
-package GameObject;
-
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 
-import Effect.Animation;
-import Effect.CacheDataLoader;
-import Effect.FrameImage;
-import UI.GameFrame;
+import Effect.*;
 
 
-public class Hero extends Creature{
-	
-	private FrameImage image1 = CacheDataLoader.getInstance().getFrameImage("up0");
-	private BufferedImage up = image1.getImage();
-	private FrameImage image2 = CacheDataLoader.getInstance().getFrameImage("down0");
-	private BufferedImage down = image2.getImage();
-	private FrameImage image3 = CacheDataLoader.getInstance().getFrameImage("left0");
-	private BufferedImage left = image3.getImage();
-	private FrameImage image4 = CacheDataLoader.getInstance().getFrameImage("right0");
-	private BufferedImage right = image4.getImage();
-	
-	private int dir = 0;
-	
+public class Hero extends Character{
+
+	public static final int beginY[] = {0, 416, 64, 192};
+	public static final int endY[] = {416, 64, 192, 416};
 	public boolean battle = false;
 	
-	//private Animation upAnim = CacheDataLoader.getInstance().getAnimation("up");
-	//private Animation downAnim = CacheDataLoader.getInstance().getAnimation("down");
-	//private Animation leftAnim = CacheDataLoader.getInstance().getAnimation("left");
-	//private Animation rightAnim = CacheDataLoader.getInstance().getAnimation("right");
-	
-	//private Item[] Items = new Item[4];
-	
-	public Hero(int mapX, int mapY, int maxHP, double speed, int luck, int attack, GameWorld gameWorld) {
-		super(mapX, mapY, maxHP, speed, luck, attack, gameWorld);
-		
-	}
-	
-	public int getDir() {
-		return dir;
-	}
+	public Hero() {}
 
-	public void setDir(int dir) {
-		this.dir = dir;
+	public Hero(String name, float mapX, float mapY, int maxHp, int attack, int luck, float movementSpeed, GameWorld gameWorld) {
+		super(name, mapX, mapY, maxHp, attack, luck, movementSpeed, gameWorld);
 	}
-
-	public void draw(Graphics2D g2, int dir) {
-		switch(dir) {
-		case 0:
-			g2.drawImage(up, getMapX(), getMapY(), null);
-			break;
-		case 1:
-			g2.drawImage(down, getMapX(), getMapY(), null);
-			break;
-		case 2:
-			g2.drawImage(left, getMapX(), getMapY(), null);
-			break;
-		case 3:
-			g2.drawImage(right, getMapX(), getMapY(), null);
-			break;
-		}
-		
-	}
-	
 	public void equip(Item item) {
 		
 	}
@@ -77,56 +29,57 @@ public class Hero extends Creature{
 		
 	}
 	
+	@Override
 	public void update() {
+		boolean canMove[] = {true, true, true, true};
 		
-	}
-	
-	public boolean canMove(int dir) {
-		switch (dir) {
-		case 0: 
-			if(getMapY() <= 0) return false;
-			if(gameWorld.map.getTile(getMapX()/32, getMapY()/32-1)==1) return false;
-			break;
-		case 1: 
-			if((getMapY()+32) >= 448) return false;
-				if(gameWorld.map.getTile(getMapX()/32, getMapY()/32+1)==1) return false;
-			break;
-		case 2:
-			if(getMapX() <= 0)  return false;
-			if(gameWorld.map.getTile(getMapX()/32-1, getMapY()/32)==1) return false;
-			break;
-		case 3:
-			if((getMapX()+32) >= 576)  return false;
-			if(gameWorld.map.getTile(getMapX()/32+1, getMapY()/32)==1) return false;
-			break;
-		default:
-			break;
-		}
-		return true;
-	}
-
-	public void move(int dir) {
-		setDir(dir);
-		switch (dir) {
-			case 0: 
-				if (canMove(dir))
-					setMapY(getMapY()-32);
-				break;
-			case 1:
-				if (canMove(dir))
-					setMapY(getMapY()+32);
-				break;
-			case 2:
-				if (canMove(dir))
-					setMapX(getMapX()-32);
-				break;
-			case 3:
+		int tileX = (int) getMapX() / 32;
+		int tileY = (int) getMapY() / 32;
+		
+		if (getMapX() % 32 == 0 || getMapY() % 32 == 0)
+			for (int i : canDir[15 - getGameWorld().map.getTile(tileX, tileY)])
+				canMove[i] = false;
+		
+		if (getMapX() % 32 == 0 && getMapY() % 32 != 0) {
+			for (int i : canDir[15 - getGameWorld().map.getTile(tileX, tileY+1)])
 				
-				if (canMove(dir))
-					setMapX(getMapX()+32);
+				canMove[i] = false;
+			canMove[UP_DIR] = true;
+			canMove[DOWN_DIR] = true;
+		}
+		
+		if (getMapX() % 32 != 0 && getMapY() % 32 == 0) {
+			for (int i : canDir[15 - getGameWorld().map.getTile(tileX+1, tileY)])
+				canMove[i] = false;
+			canMove[LEFT_DIR] = true;
+			canMove[RIGHT_DIR] = true;
+		}
+		
+		if(canMove[getDirection()])
+			switch(getDirection()) {
+			case LEFT_DIR:
+				if(getMapX()==0 && getMapY()==beginY[getGameWorld().map.getRound()-1]) {
+					getGameWorld().map.backMap();
+					setMapX(544);
+				}
+				else setMapX(getMapX() + getMovementSpeed());
 				break;
-			default: 
+			case RIGHT_DIR:
+				if(getMapX()==544 && getMapY()==endY[getGameWorld().map.getRound()-1]) {
+					getGameWorld().map.nextMap();
+					setMapX(0);
+				}
+				else setMapX(getMapX() + getMovementSpeed());
 				break;
+			case UP_DIR:
+			case DOWN_DIR:
+				setMapY(getMapY() + getMovementSpeed());
+				break;
+			}
+		else {
+			setMapX(getMapX());
+			setMapY(getMapY());
+			setMovementSpeed(0);
 		}
 	}
 }
