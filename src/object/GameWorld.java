2a28b12870417ed0fca10ba450e9cf387e736a11
pbching <80338862+pbching@users.noreply.github.com>
2021-06-11 17:26:09 +0700

    Update GameWorld.java

--- src/object/GameWorld.java
+++ src/object/GameWorld.java
@@ -7,7 +7,7 @@ public class GameWorld {
 	public Map map;
 	public Monster slime, gayzer, bat, skeleton;
 	public Hero hero;
-	public Battle b1;
+	public BattleMap b1;
 	
 	public GameWorld() {
 		map = new Map();
@@ -16,18 +16,19 @@ public class GameWorld {
 		bat = new Monster("bat", 352, 0, 100, 20, 30, 0, this, 1);
 		skeleton = new Monster("skeleton", 32, 352, 100, 20, 30, 0, this, 2);
 		hero = new Hero("hero", 0, 0, 500, 30, 50, 0, this);
-		b1 = new Battle(hero);
+		b1 = new BattleMap(hero);
 	}
 	
 	public void drawGameWorld(Graphics2D g2){
-		map.drawMap(g2, 2);
-		b1.draw(g2);
-		slime.draw(g2, 1);
-		bat.draw(g2, 1);
-		gayzer.draw(g2, 2);
-		skeleton.draw(g2, 2);
-		hero.draw(g2);
-		
+		if(hero.battle == false) {
+			map.drawMap(g2);
+			slime.draw(g2, 1);
+			bat.draw(g2, 1);
+			gayzer.draw(g2, 2);
+			skeleton.draw(g2, 2);
+			hero.draw(g2);
+		}
+		else b1.draw(g2);
 	}
 	
 	public void update() {
