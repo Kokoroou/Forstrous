bf65412baeb9829841af4fe04688f1ee400b876f
pbching <80338862+pbching@users.noreply.github.com>
2021-06-14 00:17:27 +0700

    Update InputManager.java

--- src/ui/InputManager.java
+++ src/ui/InputManager.java
@@ -5,41 +5,35 @@ import java.awt.event.KeyEvent;
 import object.*;
 
 public class InputManager {
-	private GameWorld gameWorld;
+	private GamePanel gamePanel;
 	
-	public InputManager(GameWorld gameWorld) {
-		this.gameWorld = gameWorld;
+	public InputManager(GamePanel gamePanel) {
+		this.gamePanel = gamePanel;
 	}
 	
 	public void processKeyPressed(int keyCode) {
 		switch(keyCode) {
 			case KeyEvent.VK_UP:
-				gameWorld.hero.setDirection(Hero.UP_DIR);
-				gameWorld.hero.setMovementSpeed(-4);
+				gamePanel.getGameWorld().hero.setDirection(Hero.UP_DIR);
+				gamePanel.getGameWorld().hero.setMovementSpeed(-4);
 				break;
 			case KeyEvent.VK_DOWN:
-				gameWorld.hero.setDirection(Hero.DOWN_DIR);
-				gameWorld.hero.setMovementSpeed(4);
+				gamePanel.getGameWorld().hero.setDirection(Hero.DOWN_DIR);
+				gamePanel.getGameWorld().hero.setMovementSpeed(4);
 				break;
 			case KeyEvent.VK_LEFT:
-				gameWorld.hero.setDirection(Hero.LEFT_DIR);
-				gameWorld.hero.setMovementSpeed(-4);
+				gamePanel.getGameWorld().hero.setDirection(Hero.LEFT_DIR);
+				gamePanel.getGameWorld().hero.setMovementSpeed(-4);
 				break;
 			case KeyEvent.VK_RIGHT:
-				gameWorld.hero.setDirection(Hero.RIGHT_DIR);
-				gameWorld.hero.setMovementSpeed(4);
-				break;
-			case KeyEvent.VK_1:
-				if(gameWorld.b1.isInBattle())
-				gameWorld.b1.setHeroChoice(1);
-				break;
-			case KeyEvent.VK_2:
-				if(gameWorld.b1.isInBattle())
-				gameWorld.b1.setHeroChoice(2);
+				gamePanel.getGameWorld().hero.setDirection(Hero.RIGHT_DIR);
+				gamePanel.getGameWorld().hero.setMovementSpeed(4);
 				break;
 			case KeyEvent.VK_3:
-				if(gameWorld.b1.isInBattle())
-				gameWorld.b1.setHeroChoice(3);
+				System.out.println(1);
+				if(gamePanel.getGameWorld().hero.battle)
+					gamePanel.getBattle().setHeroChoice(3);
+					
 				break;
 			default: break;
 		
@@ -52,14 +46,14 @@ public class InputManager {
 			case KeyEvent.VK_DOWN:
 			case KeyEvent.VK_LEFT:
 			case KeyEvent.VK_RIGHT:
-				gameWorld.hero.setMovementSpeed(0);
+				gamePanel.getGameWorld().hero.setMovementSpeed(0);
+				break;
+			case KeyEvent.VK_3:
+				if(gamePanel.getGameWorld().hero.battle) {
+					gamePanel.getBattle().setHeroChoice(0);
+				}
 				break;
 			default: break;
-			case KeyEvent.VK_1:
-			case KeyEvent.VK_2:
-			case KeyEvent.VK_3:
-				gameWorld.b1.setHeroChoice(0);
-				break;
 		}
 	}
 }
