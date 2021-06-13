eb72944436d7b276a14518fbc8d9dc7bffe8bfda
pbching <80338862+pbching@users.noreply.github.com>
2021-06-13 23:21:57 +0700

    Update GamePanel.java

--- src/ui/GamePanel.java
+++ src/ui/GamePanel.java
@@ -1,13 +1,16 @@
 package ui;
 
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.BitSet;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
@@ -15,25 +18,60 @@ import javax.swing.JPanel;
 import effect.*;
 import object.*;
 
-public class GamePanel extends JPanel implements Runnable, KeyListener{
-	
+public class GamePanel extends JPanel implements Runnable{
+	private static final String TAG_GAMEWORLD = "tag_gameworld";
+	private static final String TAG_BATTLE = "tag_battle";	
 	private Thread thread;
 	private boolean isRunning;
-	private InputManager inputManager;
 	private BufferedImage bufImage;
 	private Graphics2D bufG2D;
-	private GameWorld gameWorld;
+	ControlPanel control;
+	GameWorld gameWorld;
+	public Battle battle;
+	CardLayout cardLayout;
+	private BitSet traceKey = new BitSet();
 	
-	public GamePanel() {
-		gameWorld = new GameWorld();
-		inputManager = new InputManager(gameWorld);
-		bufImage = new BufferedImage(GameFrame.SCREEN_WIDTH, GameFrame.SCREEN_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+	public GamePanel(ControlPanel control) {
+		
+		this.control = control;
+		this.cardLayout = new CardLayout();
+		
+		setLayout(this.cardLayout);
+		setFocusable(true);
+		addKeyListener(keyAdapter);
+		
+		gameWorld = new GameWorld(this);
+		battle = new Battle(this);
+		bufImage = new BufferedImage(GUI.WIDTH, GUI.HEIGHT, BufferedImage.TYPE_INT_ARGB);
+		
+		this.add(this.gameWorld, TAG_GAMEWORLD);
+		this.add(this.battle, TAG_BATTLE);
+		this.addKeyListener(gameWorld);
+		this.showGameWorld();
 	}
 	
-	@Override
-	public void paint (Graphics g) {
-		g.drawImage(bufImage, 0, 0, this);
-		
+//	@Override
+//	public void paint (Graphics g) {
+//		g.drawImage(bufImage, 0, 0, this);
+//		
+//	}
+	
+	public void showGameWorld() {
+		cardLayout.show(this, TAG_GAMEWORLD);
+		gameWorld.requestFocus();
+	}
+	
+	public void showBattle() {
+		cardLayout.show(this, TAG_BATTLE);
+		battle.requestFocus();
+	}
+	
+	public ControlPanel getControl() {
+		return control;
+	}
+
+	public void setControl(ControlPanel control) {
+		this.control = control;
 	}
 	
 	public void UpdateGame() {
@@ -42,7 +80,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 	
 	public void RenderGame() {
 		if(bufImage == null)
-			bufImage = new BufferedImage(GameFrame.SCREEN_WIDTH, GameFrame.SCREEN_HEIGHT, BufferedImage.TYPE_INT_ARGB);
+			bufImage = new BufferedImage(GUI.WIDTH, GUI.HEIGHT, BufferedImage.TYPE_INT_ARGB);
 		
 		if(bufImage != null) {
 			bufG2D = (Graphics2D) bufImage.getGraphics();
@@ -50,11 +88,9 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 		
 		if(bufG2D != null) {
 			bufG2D.setColor(Color.white);
-			bufG2D.fillRect(0, 0, GameFrame.SCREEN_WIDTH, GameFrame.SCREEN_HEIGHT);
+			bufG2D.fillRect(0, 0, GUI.WIDTH, GUI.HEIGHT);
 		}
 		
-		//draw object game here
-		gameWorld.drawGameWorld(bufG2D);
 		
 	}
 	
@@ -76,7 +112,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
         long period = 1000000000/30;
 
         while(isRunning){
-
+        	
             UpdateGame();
             RenderGame();
         	
@@ -94,21 +130,18 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 
             previousTime = System.nanoTime();
         }
-
+        
     }
-
-	@Override
-	public void keyTyped(KeyEvent e) {
+	
+	private KeyAdapter keyAdapter = new KeyAdapter() {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			traceKey.set(e.getKeyCode());
+		}
 		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		inputManager.processKeyPressed(e.getKeyCode());
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		inputManager.processKeyReleaseed(e.getKeyCode());
-	}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			traceKey.clear(e.getKeyCode());
+		}
+	};
 }
